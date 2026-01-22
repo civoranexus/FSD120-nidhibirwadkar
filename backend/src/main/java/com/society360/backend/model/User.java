c823146e81924ff71a153e798a59852b81fcf03a
nidhibirwadkar <nidhibirwadkar02@gmail.com>
2026-01-22 21:15:00 +0530

    Spring Boot backend started successfully

--- backend/src/main/java/com/society360/backend/model/User.java
+++ backend/src/main/java/com/society360/backend/model/User.java
@@ -7,40 +7,5 @@ public class User {
     private String email;
     private String role;
 
-    public User() {
-        // default constructor
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
+    public User() {}
 }
-
