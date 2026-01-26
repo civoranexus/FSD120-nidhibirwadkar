77af3c19275ddf068ed6be9bd686a106d68070f0
nidhibirwadkar <nidhibirwadkar02@gmail.com>
2026-01-26 19:23:18 +0530

    Implemented User API using Spring Boot service and controller

--- backend/src/main/java/com/society360/backend/controller/UserController.java
+++ backend/src/main/java/com/society360/backend/controller/UserController.java
@@ -4,13 +4,16 @@ import com.society360.backend.model.User;
 import com.society360.backend.service.UserService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    private final UserService service = new UserService();
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
 
     @GetMapping("/users")
     public List<User> getUsers() {
--- backend/src/main/java/com/society360/backend/service/UserService.java
+++ backend/src/main/java/com/society360/backend/service/UserService.java
@@ -2,14 +2,19 @@ package com.society360.backend.service;
 
 import com.society360.backend.model.User;
 import com.society360.backend.repository.UserRepository;
+import org.springframework.stereotype.Service;
 import java.util.List;
 
+@Service
 public class UserService {
 
-    private final UserRepository repository = new UserRepository();
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
 
     public List<User> getAllUsers() {
         return repository.findAllUsers();
     }
 }
-
