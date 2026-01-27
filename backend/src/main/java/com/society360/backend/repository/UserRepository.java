157509bcb7ac66e610fba3395cc4f11ae9d93083
nidhibirwadkar <nidhibirwadkar02@gmail.com>
2026-01-27 23:33:39 +0530

    Refactored User module using Spring Boot annotations and dependency injection

--- backend/src/main/java/com/society360/backend/repository/UserRepository.java
+++ backend/src/main/java/com/society360/backend/repository/UserRepository.java
@@ -1,9 +1,12 @@
 package com.society360.backend.repository;
 
 import com.society360.backend.model.User;
+import org.springframework.stereotype.Repository;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class UserRepository {
 
     public List<User> findAllUsers() {
