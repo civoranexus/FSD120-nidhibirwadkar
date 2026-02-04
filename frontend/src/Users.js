a614dca9e1c0a4017581ff937ac01cac93624456
nidhibirwadkar <nidhibirwadkar02@gmail.com>
2026-02-04 22:28:16 +0530

    Frontend navigation and users module in progress

--- frontend/src/Users.js
+++ frontend/src/Users.js
@@ -1,5 +1,28 @@
+import { useEffect, useState } from "react";
+
 function Users() {
-  return <h2>Users Page</h2>;
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("/users")
+      .then(res => res.json())
+      .then(data => setUsers(data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h2>Users</h2>
+      <ul>
+        {users.map(user => (
+          <li key={user.id}>
+            {user.name} - {user.role} ({user.email})
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
+
 export default Users;
 
